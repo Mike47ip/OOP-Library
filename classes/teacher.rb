065a9b146cec4e29543f491c542789b999b697be
Mike Dark <agyemankojo47@gmail.com>
2023-10-10 15:23:59 +0000

    Create teacher class

---
+++ classes/teacher.rb
@@ -0,0 +1,11 @@
+class Teacher < Person
+  def initialize(name, age, specialization)
+    super(name, age)
+    @specialization = specialization
+  end
+
+  # Override can_use_services? to always return true
+  def can_use_services?
+    true
+  end
+end
