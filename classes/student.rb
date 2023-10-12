0b84a5ed78c78a6ad7292aa029ba0e50a30fc294
Mike Dark <agyemankojo47@gmail.com>
2023-10-12 14:34:57 +0000

    Remodify student class

--- classes/student.rb
+++ classes/student.rb
@@ -1,9 +1,12 @@
 require_relative 'person'
 
 class Student < Person
+  attr_accessor :classroom
+  
   def initialize(name, age, classroom)
     super(name, age)
     @classroom = classroom
+    classroom.add_student(self) # Automatically add the student to the classroom.
   end
 
   def play_hooky
