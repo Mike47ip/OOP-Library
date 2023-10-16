ca81c0f2534499b97f76f3e84327b657f97bc110
AliReza <ali.reza.buet@gmail.com>
2023-10-16 20:58:53 +0600

    Refactor: Centralize the input option and validation checks

--- classes/main.rb
+++ classes/main.rb
@@ -1,10 +1,28 @@
 require_relative 'app'
-require_relative 'initial'
+# require_relative 'initial'
 
 def main
   app = App.new
+  tasks = {
+      1 => :list_books,
+      2 => :list_people,
+      3 => :create_person,
+      4 => :create_book,
+      5 => :create_rental,
+      6 => :list_rental,
+      7 => :exit,
+      default: :invalid_option
+    }
 
-  initial(app)
+  loop do
+    app.options
+    print 'input: '
+    option = gets.chomp.to_i
+    selection = tasks[option] || tasks[:default]
+    app.send(selection)
+    puts "\n"
+  end
+
 end
 
 main
