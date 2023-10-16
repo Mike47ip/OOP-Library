a67fba725c46a56c9bd8278cf89b095eac6e7312
AliReza <ali.reza.buet@gmail.com>
2023-10-16 21:32:25 +0600

    Fix the linters

--- classes/main.rb
+++ classes/main.rb
@@ -1,30 +1,29 @@
 require_relative 'app'
 
+TASKS = {
+  1 => :list_books,
+  2 => :list_people,
+  3 => :create_person,
+  4 => :create_book,
+  5 => :create_rental,
+  6 => :list_rental,
+  default: :invalid_option
+}.freeze
+
 def main
   app = App.new
-  tasks = {
-      1 => :list_books,
-      2 => :list_people,
-      3 => :create_person,
-      4 => :create_book,
-      5 => :create_rental,
-      6 => :list_rental,
-      default: :invalid_option
-    }
-
   loop do
     app.options
     print 'input: '
     option = gets.chomp.to_i
-    if option==7
-      puts "Exiting... Thank you"
+    if option == 7
+      puts 'Exiting... Thank you'
       break
     end
-    selection = tasks[option] || tasks[:default]
+    selection = TASKS[option] || TASKS[:default]
     app.send(selection)
     puts "\n"
   end
-
 end
 
 main
