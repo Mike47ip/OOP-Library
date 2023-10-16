5a020a984ee1c418c7a67560bc96024cd13f0988
Michael Darkwah <agyemankojo47@gmail.com>
2023-10-16 10:34:55 +0000

    Revert "Setup associations"

--- classes/person.rb
+++ classes/person.rb
@@ -1,16 +1,15 @@
 require_relative 'nameable'
 
 class Person < Nameable
-  attr_accessor :name, :age, :rentals
+  attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age:, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = id
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @rentals = [] # Initialize an empty array for rentals
   end
 
   def can_use_services?
@@ -21,11 +20,6 @@ class Person < Nameable
     @name
   end
 
-  # Method to associate a rental with a person
-  def rent_book(book, date)
-    rental = Rental.new(date, self, book)
-    @rentals << rental
-  end
   private
 
   def of_age?
